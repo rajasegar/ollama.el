96badb5131a37c03418fd3932bc11f7d661fe534
Rajasegar Chandran <rajasegar.c@gmail.com>
2024-12-08 20:23:02 +0530

    chore: add new functions to run models

--- ollama.el
+++ ollama.el
@@ -1,5 +1,7 @@
 ;; ollama.el
 
+(defvar *ollama-models* '("qwen2.5:0.5b" "qwen2.5-coder:1.5b" "llama3.2:1b"))
+
 (defun ollama-list ()
   "List models"
   (interactive)
@@ -14,12 +16,6 @@
 
 ;; (ollama-show "llama3.2")
 
-(defun ollama-run (model)
-  "Run a model"
-  (interactive)
-  (async-shell-command (format "ollama run %s" model)))
-
-;; (ollama-run "llama3.2")
 
 (defun ollama-ps ()
   "List running models"
@@ -55,4 +51,33 @@
 
 (message (replace-regexp-in-string "ollama version is " "" "ollama version is 0.3.14"))
 
+
+
+(defun ollama()
+  "Run a ollama mode"
+  (interactive)
+  (let ((prompt (read-string "What is the prompt? ")))
+    (async-shell-command
+     (format "ollama run %s %s" (ido-completing-read "Select a model: " *ollama-models*) prompt)))
+  )
+
+(defun ollama-run(model prompt)
+  "Run a ollama mode"
+    (async-shell-command
+     (format "ollama run %s %s" model prompt)))
+
+
+(defun ollama-ask()
+  "Ask a question for ollama"
+  (interactive)
+  (let ((prompt (read-string "What is the question? ")))
+    (ollama-run "qwen2.5:0.5b" prompt)
+    ))
+
+(defun ollama-code()
+  "Ask a coding question for ollama"
+  (interactive)
+  (let ((prompt (read-string "What is the problem? ")))
+    (ollama-run "qwen2.5-coder:1.5b" prompt)
+    ))
 
